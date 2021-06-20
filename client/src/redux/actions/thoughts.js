4edd6c8a54b81e998fcfbf2263807eaf4a50591f
Saatvik Ramani <saatvikramani777@gmail.com>
2021-06-20 19:39:57 +0530

    refactored actions

--- client/src/redux/actions/thoughts.js
+++ client/src/redux/actions/thoughts.js
@@ -1,41 +1,25 @@
 import * as api from '../api/index'
 
 export const getThoughts = () => async (dispatch) => {
-    try {
-        const { data } = await api.getThoughts()
-
-        dispatch({ type: 'FETCH_ALL', payload: data })
-    } catch (error) {
-        console.log(error)
-    }
+    api.getThoughts()
+        .then(({ data }) => dispatch({ type: 'FETCH_ALL', payload: data }))
+        .catch(error => console.log(error))
 }
 
 export const createThought = (newThought) => async (dispatch) => {
-    try {
-        const { data } = await api.createThought(newThought)
-
-        dispatch({ type: 'CREATE', payload: data })
-    } catch (error) {
-        console.log(error)
-    }
+    api.createThought(newThought)
+        .then(({ data }) => dispatch({ type: 'CREATE', payload: data }))
+        .catch(error => console.log(error))
 }
 
 export const updateThought = (id, updatedThought) => async (dispatch) => {
-    try {
-        const { data } = await api.updateThought(id, updatedThought)
-
-        dispatch({ type: 'UPDATE', payload: data})
-    } catch (error) {
-        console.log(error)
-    }
+    api.updateThought(id, updatedThought)
+        .then(({ data }) => dispatch({ type: 'UPDATE', payload: data }))
+        .catch(error => console.log(error))
 }
 
 export const deleteThought = (id) => async (dispatch) => {
-    try {
-        await api.deleteThought(id)
-
-        dispatch({ type: 'DELETE', payload: id })
-    } catch (error) {
-        console.log(error)
-    }
+    api.deleteThought(id)
+        .then(() => dispatch({ type: 'DELETE', payload: id }))
+        .catch(error => console.log(error))
 }
